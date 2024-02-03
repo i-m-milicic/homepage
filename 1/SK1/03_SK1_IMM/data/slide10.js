c23bb7b0502bc92335556bbd8b48438c132dfdd5
i-m-milicic <158686242+i-m-milicic@users.noreply.github.com>
2024-02-03 22:06:16 +0100

    Add files via upload

---
+++ 1/SK1/03_SK1_IMM/data/slide10.js
@@ -0,0 +1,19 @@
+(function(){var loadHandler=window['sl_{A10464DA-2F22-46BF-A1F8-1CEBA710CAF1}'];loadHandler&&loadHandler(9, '<div id="spr0_1651c7"><div id="spr1_1651c7" class="kern slide"><img id="img9_1651c7" src="data/img3.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/><div id="svg0_1651c7" style="top:129.6px;"><svg width="1280" height="34" viewBox="0 0 1280 34"><path fill="#ffffff" d="M0,0 h1280 v33.6 h-1280 Z"/></svg></div><div id="svg1_1651c7" style="top:134.4px;"><svg width="75" height="24" viewBox="0 0 75 24"><path fill="#f3a447" d="M0,0 h74.667 v24 h-74.667 Z"/></svg></div><div id="svg2_1651c7" style="left:82.667px;top:134.4px;"><svg width="1198" height="24" viewBox="0 0 1198 24"><path fill="#a5b592" d="M0,0 h1197.333 v24 h-1197.333 Z"/></svg></div></div><div id="spr2_1651c7" class="kern slide"><div id="svg3_1651c7" style="left:659.087px;top:9.851px;"><svg width="589" height="121" viewBox="-1 -1 589 121"><path fill="#ffffff" stroke="#a5b592" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round" d="M1,1 h584.877 v116.325 h-584.877 Z"/></svg></div><div id="spr3_1651c7" style="left:671.25px;top:29.25px;"><img id="img0_1651c7" src="data/img7.png" width="554.25" height="92.25" alt=" 𝑧 𝑜 – број ослонаца, 	 𝑧 𝑠  – број штапова,\
+ 𝑧 𝑢  – број укљештења, 	 𝑧 𝑘  – број крутих углова,\
+𝑘 – број чворова"/></div><div id="svg4_1651c7" style="left:85.76px;top:652.909px;"><svg width="1142" height="49" viewBox="0 0 1142 49"><path fill="#a5b592" d="M0,0 h1141.333 v48.469 h-1141.333 Z"/></svg></div><div id="spr4_1651c7" style="left:85.76px;top:652.909px;"><div style="width:0px;"><span id="txt0_1651c7" data-width="1099.343750" style="left:23.58px;top:6.424px;">Број елемената носача је једнозначно одређен када су усвојени чворови.</span></div></div><div id="spr5_1651c7" style="left:522.75px;top:209.25px;"><img id="img1_1651c7" src="data/img8.png" width="85.5" height="144.75" alt=" 𝑧 𝑜 =3\
+ 𝑧 𝑢 =0\
+ 𝑧 𝑠 =25\
+ 𝑧 𝑘 =0\
+𝑘=14"/></div><div id="spr6_1651c7" style="left:522.75px;top:453.75px;"><img id="img2_1651c7" src="data/img9.png" width="86.25" height="144.75" alt=" 𝑧 𝑜 =4\
+ 𝑧 𝑢 =0\
+ 𝑧 𝑠 =26\
+ 𝑧 𝑘 =0\
+𝑘=15"/></div><div id="spr7_1651c7" style="left:1122.75px;top:208.5px;"><img id="img3_1651c7" src="data/img10.png" width="75" height="144.75" alt=" 𝑧 𝑜 =3\
+ 𝑧 𝑢 =0\
+ 𝑧 𝑠 =3 \
+ 𝑧 𝑘 =2\
+𝑘=4"/></div><div id="spr8_1651c7" style="left:1122.75px;top:451.5px;"><img id="img4_1651c7" src="data/img11.png" width="88.5" height="144.75" alt=" 𝑧 𝑜 =7\
+ 𝑧 𝑢 =2\
+ 𝑧 𝑠 =11\
+ 𝑧 𝑘 =12\
+𝑘=10"/></div><div id="spr9_1651c7" style="left:85.76px;top:24px;"><div style="width:0px;"><span id="txt1_1651c7" data-width="190.541672" style="left:9.6px;top:28.565px;">Примери</span></div></div><div id="spr10_1651c7" style="left:77.985px;top:188.838px;"><img id="img5_1651c7" src="data/img12.png" width="378.75" height="173.25" alt="" style="left:0.114px;top:-0.085px;"/></div><div id="spr11_1651c7" style="left:76.121px;top:423.841px;"><img id="img6_1651c7" src="data/img13.png" width="386.25" height="197.25" alt="" style="left:-0.074px;top:-0.083px;"/></div><div id="spr12_1651c7" style="left:702.178px;top:176.198px;"><img id="img7_1651c7" src="data/img14.png" width="348" height="162.75" alt="" style="left:0.018px;top:0.102px;"/></div><div id="spr13_1651c7" style="left:690.091px;top:423.841px;"><img id="img8_1651c7" src="data/img15.png" width="351.75" height="165" alt="" style="left:0.112px;top:0.009px;"/></div></div></div>');})();
