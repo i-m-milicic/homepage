6d6d37d9b2b9b20b373d3c902e36c78d13e0f45c
i-m-milicic <158686242+i-m-milicic@users.noreply.github.com>
2024-02-03 18:31:18 +0100

    Add files via upload

---
+++ 1/KS/13_KS_IMM/data/slide11.js
@@ -0,0 +1,14 @@
+(function(){var loadHandler=window['sl_{7403C685-60E1-434F-A4DC-F07343EC00D2}'];loadHandler&&loadHandler(10, '<div id="spr0_51456a"><div id="spr1_51456a" class="kern slide"><img id="img10_51456a" src="data/img16.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_51456a" class="kern slide"><div id="spr3_51456a" style="left:79.996px;top:99.998px;"><div style="width:0px;"><span id="txt0_51456a" data-width="350.917358" style="left:9.6px;top:1.201px;">E sad daš mu neki tekst za test</span></div></div><div id="spr4_51456a"><div id="spr5_51456a"><img id="img0_51456a" src="data/img7.jpg" width="1279.5" height="720" alt="Slajd.jpg" style="left:0.188px;"/></div><img id="img1_51456a" src="data/img8.png" width="1280" height="720" alt=""/></div><div id="spr6_51456a" style="left:39.996px;top:39.998px;"><div id="spr7_51456a" style="left:-1.746px;top:-1.748px;"><img id="img2_51456a" src="data/img17.png" width="45" height="37.5" alt=""/></div><div id="spr8_51456a" style="left:48.504px;top:-1.748px;"><img id="img3_51456a" src="data/img18.png" width="45" height="37.5" alt=""/></div><div id="spr9_51456a" style="left:98.004px;top:-1.748px;"><img id="img4_51456a" src="data/img19.png" width="45.75" height="37.5" alt=""/></div><div id="spr10_51456a" style="left:148.254px;top:-1.748px;"><img id="img5_51456a" src="data/img20.png" width="45" height="37.5" alt=""/></div></div><div id="spr11_51456a" style="left:352.5px;top:38.25px;"><img id="img6_51456a" src="data/img29.png" width="548.25" height="45" alt="Konstrukcija drumskog mosta"/></div><div id="spr12_51456a" style="left:-13.167px;top:73.5px;"><img id="img7_51456a" src="data/img14.png" width="1305" height="18" alt=""/></div><div id="spr13_51456a" style="left:-13.167px;top:693.75px;"><img id="img8_51456a" src="data/img14.png" width="1305" height="18" alt=""/></div><div id="spr14_51456a" style="left:41.25px;top:139.5px;"><img id="img9_51456a" src="data/img30.png" width="1176.75" height="544.5" alt="Most je drumski od armiranog betona i spada u gredni sistem.\
+\
+ Osnovni elementi:\
+Kolovozna ploča je olakšana od AB adheziono prednapregnuta u \
+    poprečnom pravcu mosta.\
+Glavni nosači su AB kontinualne grede na tri polja sandučastog \
+         poprečnog preseka. Odnos raspona je: 1 – 1,5 – 1,\
+Stubovi su od AB (trokraki) pravougaonog poprečnog preseka,\
+Temelji i temeljne grede od AB pravougaonog poprečnog preseka.\
+ Dopunski elementi:\
+ograda mosta,\
+slivnici,\
+otvori za instalacije i opremu za monitoring konstrukcije,\
+pristupna stepeništa,…"/></div></div></div>');})();
---
+++ 1/KS/13_KS_IMM/data/slide8.js
@@ -0,0 +1,14 @@
+(function(){var loadHandler=window['sl_{7403C685-60E1-434F-A4DC-F07343EC00D2}'];loadHandler&&loadHandler(7, '<div id="spr0_512c20"><div id="spr1_512c20" class="kern slide"><img id="img10_512c20" src="data/img16.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/></div><div id="spr2_512c20" class="kern slide"><div id="spr3_512c20" style="left:79.996px;top:99.998px;"><div style="width:0px;"><span id="txt0_512c20" data-width="350.917358" style="left:9.6px;top:1.201px;">E sad daš mu neki tekst za test</span></div></div><div id="spr4_512c20"><div id="spr5_512c20"><img id="img0_512c20" src="data/img7.jpg" width="1279.5" height="720" alt="Slajd.jpg" style="left:0.188px;"/></div><img id="img1_512c20" src="data/img8.png" width="1280" height="720" alt=""/></div><div id="spr6_512c20" style="left:39.996px;top:39.998px;"><div id="spr7_512c20" style="left:-1.746px;top:-1.748px;"><img id="img2_512c20" src="data/img17.png" width="45" height="37.5" alt=""/></div><div id="spr8_512c20" style="left:48.504px;top:-1.748px;"><img id="img3_512c20" src="data/img18.png" width="45" height="37.5" alt=""/></div><div id="spr9_512c20" style="left:98.004px;top:-1.748px;"><img id="img4_512c20" src="data/img19.png" width="45.75" height="37.5" alt=""/></div><div id="spr10_512c20" style="left:148.254px;top:-1.748px;"><img id="img5_512c20" src="data/img20.png" width="45" height="37.5" alt=""/></div></div><div id="spr11_512c20" style="left:540px;top:38.25px;"><img id="img6_512c20" src="data/img21.png" width="141" height="39" alt="Rezime"/></div><div id="spr12_512c20" style="left:-13.167px;top:73.5px;"><img id="img7_512c20" src="data/img14.png" width="1305" height="18" alt=""/></div><div id="spr13_512c20" style="left:-13.167px;top:693.75px;"><img id="img8_512c20" src="data/img14.png" width="1305" height="18" alt=""/></div><div id="spr14_512c20" style="left:52.5px;top:127.5px;"><img id="img9_512c20" src="data/img22.png" width="1123.5" height="468" alt=" U radu je razmatrana:\
+\
+•  Lokacija i idejno rešenje konstrukcije drumskog mosta,\
+•  Osnovni elementi mostovske konstrukcije (srednji otvor mosta),\
+•  Analiza opterećenja,\
+•  Računski modeli (2D i 3D),\
+•  Poređenja kriterijuma upotrebljivosti drumskog mosta L/500 i  \
+         L/800 preko količina materijala,\
+•  Poređenja graničnih stanja upotrebljivosti (GSU) prema:\
+SRPS (JUS) standardu, i \
+Eurocode.\
+•  Odnos sopstvene težine i pokretnog (korisnog) opterećenja.\
+\
+"/></div></div></div>');})();
