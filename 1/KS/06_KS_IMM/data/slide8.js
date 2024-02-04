66269cae9c33f592b43e55706eea63f04233d7b8
i-m-milicic <158686242+i-m-milicic@users.noreply.github.com>
2024-02-04 04:38:51 +0100

    Add files via upload

---
+++ 1/KS/06_KS_IMM/data/slide8.js
@@ -0,0 +1,19 @@
+(function(){var loadHandler=window['sl_{EB990A7C-B9CB-4395-91D4-6C9E7B9880DC}'];loadHandler&&loadHandler(7, '<div id="spr0_4015c2"><div id="spr1_4015c2" class="kern slide"><img id="img9_4015c2" src="data/img1.png" width="1280px" height="720px" alt="" style="left:0px;top:0px;"/><div id="svg0_4015c2" style="top:153.305px;"><svg width="1280" height="34" viewBox="0 0 1280 34"><path fill="#ffffff" d="M0,0 h1280 v33.6 h-1280 Z"/></svg></div><div id="svg1_4015c2" style="top:158.105px;"><svg width="75" height="24" viewBox="0 0 75 24"><path fill="#da1f28" d="M0,0 h74.667 v24 h-74.667 Z"/></svg></div><div id="svg2_4015c2" style="left:82.667px;top:158.105px;"><svg width="1198" height="24" viewBox="0 0 1198 24"><path fill="#2da2bf" d="M0,0 h1197.333 v24 h-1197.333 Z"/></svg></div></div><div id="spr2_4015c2" class="kern slide"><div id="spr3_4015c2" style="left:85.76px;top:24px;"><div style="width:0px;"><span id="txt0_4015c2" data-width="178.854172" style="left:12.8px;top:78.753px;">Примери</span></div></div><div id="svg3_4015c2" style="left:654.427px;top:22px;"><svg width="589" height="121" viewBox="-1 -1 589 121"><path fill="#ffffff" stroke="#2da2bf" stroke-width="2" stroke-linecap="butt" stroke-linejoin="round" d="M1,1 h584.877 v116.325 h-584.877 Z"/></svg></div><div id="spr4_4015c2" style="left:666.75px;top:41.25px;"><img id="img0_4015c2" src="data/img5.png" width="554.25" height="92.25" alt=" 𝑧 𝑜 – број ослонаца, 	 𝑧 𝑠  – број штапова,\
+ 𝑧 𝑢  – број укљештења, 	 𝑧 𝑘  – број крутих углова,\
+𝑘 – број чворова"/></div><div id="svg4_4015c2" style="left:85.76px;top:652.909px;"><svg width="1142" height="49" viewBox="0 0 1142 49"><path fill="#2da2bf" d="M0,0 h1141.333 v48.469 h-1141.333 Z"/></svg></div><div id="spr5_4015c2" style="left:85.76px;top:652.909px;"><div style="width:0px;"><span id="txt1_4015c2" data-width="1099.343750" style="left:23.58px;top:6.424px;">Број елемената носача је једнозначно одређен када су усвојени чворови.</span></div></div><div id="spr6_4015c2" style="left:522.75px;top:224.25px;"><img id="img1_4015c2" src="data/img6.png" width="85.5" height="145.5" alt=" 𝑧 𝑜 =3\
+ 𝑧 𝑢 =0\
+ 𝑧 𝑠 =25\
+ 𝑧 𝑘 =0\
+𝑘=14"/></div><div id="spr7_4015c2" style="left:522.75px;top:468px;"><img id="img2_4015c2" src="data/img7.png" width="86.25" height="145.5" alt=" 𝑧 𝑜 =4\
+ 𝑧 𝑢 =0\
+ 𝑧 𝑠 =26\
+ 𝑧 𝑘 =0\
+𝑘=15"/></div><div id="spr8_4015c2" style="left:1122.75px;top:222.75px;"><img id="img3_4015c2" src="data/img8.png" width="75" height="145.5" alt=" 𝑧 𝑜 =3\
+ 𝑧 𝑢 =0\
+ 𝑧 𝑠 =3 \
+ 𝑧 𝑘 =2\
+𝑘=4"/></div><div id="spr9_4015c2" style="left:1122.75px;top:465.75px;"><img id="img4_4015c2" src="data/img9.png" width="88.5" height="145.5" alt=" 𝑧 𝑜 =7\
+ 𝑧 𝑢 =2\
+ 𝑧 𝑠 =11\
+ 𝑧 𝑘 =12\
+𝑘=10"/></div><div id="spr10_4015c2" style="left:77.985px;top:203.329px;"><img id="img5_4015c2" src="data/img10.png" width="378.75" height="173.25" alt="" style="left:0.114px;top:-0.085px;"/></div><div id="spr11_4015c2" style="left:76.121px;top:438.331px;"><img id="img6_4015c2" src="data/img11.png" width="386.25" height="197.25" alt="" style="left:-0.074px;top:-0.083px;"/></div><div id="spr12_4015c2" style="left:702.178px;top:190.688px;"><img id="img7_4015c2" src="data/img12.png" width="348" height="162.75" alt="" style="left:0.018px;top:0.102px;"/></div><div id="spr13_4015c2" style="left:690.091px;top:438.331px;"><img id="img8_4015c2" src="data/img13.png" width="351.75" height="165" alt="" style="left:0.112px;top:0.009px;"/></div></div></div>');})();
