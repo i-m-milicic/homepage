86e03aa70d24356eaaf40dec3f29d36349961f6b
Prof. dr Ilija M. MILIČIĆ <158686242+i-m-milicic@users.noreply.github.com>
2024-11-13 02:43:30 +0100

    Add files via upload

---
+++ 1/SK1/SK1_vezbe_2024/Ispiti/mobile/javascript/bookmark_config.js
@@ -0,0 +1,1 @@
+﻿var ols=[{caption:"Image (53)",page:"1",url:"",level:"1",children:[]},{caption:"Image(54)",page:"2",url:"",level:"1",children:[]},{caption:"Image (55)",page:"3",url:"",level:"1",children:[]},{caption:"Image (56)",page:"4",url:"",level:"1",children:[]},{caption:"Image (57)",page:"5",url:"",level:"1",children:[]},{caption:"Image (58)",page:"6",url:"",level:"1",children:[]}];
